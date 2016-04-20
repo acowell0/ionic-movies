0a4321250d53bb1a94176d6f484b966b6f4f576b
Ash Cowell <a.cowell@gmail.com>
2016-04-20 11:40:50 -0500

    add details page

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,19 @@
-var movieApp = angular.module('movieApp', ['ionic', 'movieControllers', 'movieServices']);
+var movieApp = angular.module('movieApp', ['ionic', 'movieControllers', 'movieServices', 'ui.router']);
+
+movieApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('home', {
+          url:'/',
+          templateUrl: 'templates/home.html'
+      })
+      .state('details', {
+          url:'/details/{movie:json}',
+          templateUrl: '/templates/details.html',
+          controller: function($scope, $stateParams){
+            $scope.movie = $stateParams.movie;
+          }
+      });
+
+}]);
--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ var movieServices = angular.module('movieServices', []);
 movieServices.factory('movieList', ['$http', function($http){
 
     var urlBase = 'https://api.themoviedb.org/3/movie/popular?api_key=';
-    var urlApiKey = '###################';
+    var urlApiKey = 'a9b366d5e91b5c67304ef5c07a392b92';
     var movieList = {};
 
     movieList.getMovies = function() {
